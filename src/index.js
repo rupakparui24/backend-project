aac31c0415c259b65601aeec41c99d59c11f48e6
Rupak Parui <catch2rupak@gmail.com>
2025-06-26 14:05:53 +0530

    write utils functions

--- src/index.js
+++ src/index.js
@@ -6,4 +6,17 @@ dotenv.config({
 });
 
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.on("error", (err) => {
+        console.log("Server Error: ", err);
+        throw err;
+    });
+    app.listen (process.env.PORT || 8000,() => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+
+.catch((err) => {
+    console.log("Mino DB connection Failed: ",err)
+})
